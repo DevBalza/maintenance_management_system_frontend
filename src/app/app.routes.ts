23ed71af4ba325a3b91482503a3e8ef008b618d7
DevBalza <balza732@gmail.com>
2024-09-23 15:07:24 -0500

    se agrego dashboard,layout,components,shared,services y la paginacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./domain/dashboard/dashboard.component'),
+      },
+
+      {
+        path: 'profile',
+        loadComponent: () => import('./domain/profile/profile.component'),
+      },
+
+      {
+        path: 'table',
+        loadComponent: () => import('./domain/table/table.component'),
+      },
+
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
